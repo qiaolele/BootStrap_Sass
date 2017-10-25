d3721c72ca492ad854a9af62a7605e8706cba875
qiaolele <2046882662@qq.com>
2017-10-25 14:53:07 +0800

    bootstrap、scss、waves做的nav、footerand tab demo；

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+    // 头部信息显示隐藏
+    $(".header-infoBg").click(function(){
+        $(".header-infoList").toggle();
+    });
+    // 头部链接点击变色
+    $(".header-nav li a").click(function(){
+        $(".header-nav li a").removeClass('getBgColor');
+        $(this).addClass('getBgColor');
+
+    })
+})
